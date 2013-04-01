fc16665930a55e795cb818ef8684e938a193bd13
Zhong Jianxin <azuwis@gmail.com>
2013-04-01 20:50:57 +0800

    fetch subtitle from shooter.cn

--- omxplayerd.py
+++ omxplayerd.py
@@ -144,8 +144,16 @@ def prepare_subtitle(file):
     fileprefix,_ = os.path.splitext(file)
     fileprefix = os.path.join(MEDIA_RDIR, fileprefix)
     used_srt = fileprefix + '.srt'
+    srts = glob.glob(fileprefix + '*.srt')
+    if len(srts) == 0:
+        filepath_withmedia = os.path.join(MEDIA_RDIR,file)
+        shooter_subs = fetch_shooter(filepath_withmedia)
+        if shooter_subs:
+            force_utf8_and_filter_duplicates(shooter_subs)
+            save_to_disk(shooter_subs, filepath_withmedia)
+            srts = glob.glob(fileprefix + '*.srt')
     # omxplayer use exactly matched .srt subtitle, rename the first globed .srt to that
-    for srt in glob.glob(fileprefix + '*.srt'):
+    for srt in srts:
         os.rename(srt, used_srt)
         break
     if os.path.exists(used_srt):
@@ -298,3 +306,150 @@ def guess_locale_and_convert(stream):
     if not enc in ['utf_8', 'ascii']:
         stream = stream.decode(enc,'ignore').encode('utf_8')
     return enc,lang,stream
+
+def parse_shooter_package(fileobj):
+    '''Parse shooter returned package of subtitles.
+    Return subtitles encoded in UTF-8.
+    '''
+    subtitles = []
+    f = fileobj
+
+    # read contents
+    import struct, io
+    c = f.read(1)
+    package_count = struct.unpack(b'!b', c)[0]
+
+    for i in range(package_count):
+        # NOTE: '_' is the length of following byte-stream
+        c = f.read(8)
+        _,desc_length = struct.unpack(b'!II', c)
+        description = f.read(desc_length).decode('utf_8')
+        sub_delay = float(description.partition('=')[2]) / 1000.0 if description and 'delay' in description else 0
+        if description:
+            print('Subtitle description: {0}'.format(description))
+
+        c = f.read(5)
+        _,file_count = struct.unpack(b'!IB', c)
+            
+        for j in range(file_count):
+            c = f.read(8)
+            _,ext_len = struct.unpack(b'!II', c)
+            ext = f.read(ext_len)
+
+            c = f.read(4)
+            file_len = struct.unpack(b'!I', c)[0]
+            sub = f.read(file_len)
+            if sub.startswith(b'\x1f\x8b'):
+                import gzip
+                sub = gzip.GzipFile(fileobj=io.BytesIO(sub)).read()
+
+            subtitles.append({'extension': ext,
+                              'delay': sub_delay,
+                              'content': sub})
+
+    print('{0} subtitle(s) fetched.'.format(len(subtitles)))
+    return subtitles
+
+def fetch_shooter(filepath):
+    import hashlib
+    filehash = ''
+    sz = os.path.getsize(filepath)
+    if sz>8192:
+        with open(filepath,'rb') as f:
+            filehash = ';'.join([(f.seek(s), hashlib.md5(f.read(4096)).hexdigest())[1] for s in (lambda l:[4096, l/3*2, l/3, l-8192])(sz)])
+    import httplib
+    schemas = ['http', 'https'] if hasattr(httplib, 'HTTPS') else ['http']
+    servers = ['www', 'splayer', 'svplayer'] + ['splayer'+str(i) for i in range(1,13)]
+    splayer_rev = 2437 # as of 2012-07-02
+    tries = [2, 10, 30, 60, 120]
+
+    # generate data for submission
+    # shooter.cn uses UTF-8.
+    head,tail = os.path.split(filepath)
+    pathinfo = '\\'.join(['D:', os.path.basename(head), tail])
+    v_fingerpint = b'SP,aerSP,aer {0} &e(\xd7\x02 {1} {2}'.format(splayer_rev, pathinfo.encode('utf_8'), filehash.encode('utf_8'))
+    vhash = hashlib.md5(v_fingerpint).hexdigest()
+    import random
+    boundary = '-'*28 + '{0:x}'.format(random.getrandbits(48))
+
+    header = [('User-Agent',   'SPlayer Build {0}'.format(splayer_rev)),
+              ('Content-Type', 'multipart/form-data; boundary={0}'.format(boundary))
+              ]
+    items = [('filehash', filehash), ('pathinfo', pathinfo), ('vhash', vhash)]
+    data = ''.join(['--{0}\n'
+                    'Content-Disposition: form-data; name="{1}"\n\n'
+                    '{2}\n'.format(boundary, *d) for d in items]
+                   + ['--' + boundary + '--'])
+
+    # fetch
+    import urllib2
+    for i, t in enumerate(tries):
+        try:
+            print('Wait for {0}s to reconnect (Try {1} of {2})...'.format(t,i+1,len(tries)+1))
+            time.sleep(t)
+
+            url = '{0}://{1}.shooter.cn/api/subapi.php'.format(random.choice(schemas), random.choice(servers))
+
+            # shooter.cn uses UTF-8.
+            req = urllib2.Request(url.encode('utf_8'))
+            for h in header:
+                req.add_header(h[0].encode('utf_8'), h[1].encode('utf_8'))
+            req.add_data(data.encode('utf_8'))
+
+            print('Connecting server {0} with the submission:\n'
+                          '\n{1}\n'
+                          '{2}\n'.format(url,
+                                         '\n'.join(['{0}:{1}'.format(*h) for h in header]),
+                                         data))
+
+            # todo: with context manager
+            response = urllib2.urlopen(req)
+            fetched_subtitles = parse_shooter_package(response)
+            response.close()
+
+            if fetched_subtitles:
+                break
+        except urllib2.URLError, e:
+            print(e)
+    return fetched_subtitles
+
+def force_utf8_and_filter_duplicates(subtitles):
+    print('Trying to filter duplicated subtitles...')
+
+    for s in subtitles:
+        _,s['lang'],s['content'] = guess_locale_and_convert(s['content'])
+            
+    dup_tag = [False]*len(subtitles)
+    for i in range(len(subtitles)):
+        if dup_tag[i]:
+            continue
+        for j in range(i+1, len(subtitles)):
+            sa = subtitles[i]
+            sb = subtitles[j]
+            if sa['extension'] != sb['extension'] or sa['lang'] != sb['lang']:
+                continue
+            import difflib
+            similarity = difflib.SequenceMatcher(None, sa['content'], sb['content']).real_quick_ratio()
+            print('Similarity is {0}.'.format(similarity))
+            if similarity > 0.9:
+                dup_tag[j] = True
+    # TODO: reserve longer subtitles 
+    subtitles = [subtitles[i] for i in range(len(subtitles)) if not dup_tag[i]]
+    print('{0} subtitle(s) reserved after duplicates filtering.'.format(len(subtitles)))
+
+def save_to_disk(subtitles, filepath, save_dir=None):
+    prefix,_ = os.path.splitext(filepath)
+    if save_dir:
+        prefix = os.path.join(save_dir, os.path.basename(prefix))
+
+    # save subtitles
+    for s in subtitles:
+        suffix = '.' + s['lang'] if not s['lang'] == 'und' else ''
+                
+        path = prefix + suffix + '.' + s['extension']
+        if os.path.exists(path):
+            path = prefix + suffix + '1.' + s['extension']
+        with open(path,'wb') as f:
+            f.write(s['content'])
+            print('Saved the subtitle as {0}'.format(path))
+            s['path'] = path
