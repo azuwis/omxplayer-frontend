c8d980ea92f910c8233b62b9f6f1c664ed0828e0
Zhong Jianxin <azuwis@gmail.com>
2013-04-24 22:24:06 +0800

    fix online.sh when download segments are small

--- online.sh
+++ online.sh
@@ -6,7 +6,7 @@ play_if_big() {
 	file="$1"
 	while true
 	do
-		if [ $(stat -c%s "$file") -gt 20971520 ] || ! tmux list-panes -F '#{pane_id}' | grep -qFx "$youget_id" ; then
+		if [ $(stat -c%s "$file") -gt 20971520 ] || ! tmux list-panes -F '#{pane_id}' | grep -qFx "$youget_id" || (echo "$file" | grep -qF '[00].' && [ -e "${file/\[00\]\./[01].}" ]); then
 			if echo "$file" | grep -qF '[00].'; then
 				omxplayer --loop --loop-once -o hdmi "$(echo "$file" | sed -e 's/\[[0-9][0-9]\]\./[%02d]./')" <omxin &
 			else
@@ -23,7 +23,7 @@ touch "$online_dir/timestamp"
 pane_ids=`tmux list-panes -F '#{pane_id}'`
 tmux split-window -hd 'echo "downloading '$online_url'..."; cd "'$online_dir'"; you-get -n --no-suffix "'$online_url'" || sleep 5'
 youget_id=`tmux list-panes -F '#{pane_id}' | grep -Fxv "$pane_ids"`
-for i in {0..5}
+for i in {0..15}
 do
 	newfile=`find "$online_dir" -type f -newer "$online_dir/timestamp"`
 	if [ -n "$newfile" ]; then
